10c9d381518a141d63c13d6a2cafe6b35f0a87d9
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-07-12 00:22:28 -0300

    feat: bcrypt service

---
+++ src/bcrypt/bcrypt.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { compare, hash } from 'bcrypt';
+import { EncoderService } from 'src/common/abstractions/encoder.interface';
+
+@Injectable()
+export class BcryptService implements EncoderService {
+  async encode(value: string) {
+    return hash(value, 2);
+  }
+  async compare(encodedValue: string, plainValue: string) {
+    return compare(plainValue, encodedValue);
+  }
+}
