3fe6541dfde1e9dac340cd962a206ddb02da4973
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-07-12 00:24:09 -0300

    chore: app module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,54 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CoursesModule } from './courses/courses.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonsModule } from './lessons/lessons.module';
+import { VercelCdnService } from './vercel-cdn/vercel-cdn.service';
+import { ConfigModule } from '@nestjs/config';
+import { NodemailerService } from './nodemailer/nodemailer.service';
+import { AuthModule } from './auth/auth.module';
+import { BcryptService } from './bcrypt/bcrypt.service';
+import { BullModule } from '@nestjs/bullmq';
+import { join } from 'path';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), './schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(process.cwd(), '.env'),
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.BULL_REDIS_HOST,
+        port: parseInt(process.env.BULL_REDIS_PORT!),
+        username: process.env.BULL_REDIS_USERNAME,
+        password: process.env.BULL_REDIS_PASSWORD,
+      },
+      defaultJobOptions: {
+        attempts: 1,
+        removeOnFail: true,
+      },
+    }),
+    CoursesModule,
+    ReviewsModule,
+    UsersModule,
+    LessonsModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [VercelCdnService, NodemailerService, BcryptService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.use(cookieParser());
+  app.enableCors({
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
